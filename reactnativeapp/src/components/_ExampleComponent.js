5bf19da50c759f4990b18fd3b0896b6665e814c5
Carlos Aiello <carlos.aiello@globant.com>
2018-02-19 16:43:52 -0300

    Creating our first component.

---
+++ reactnativeapp/src/components/_ExampleComponent.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {
+    Text,
+    View
+  } from 'react-native';
+
+const ExampleComponent = (props) => {
+    return (
+        <View>
+            <Text>This is an example component</Text>
+        </View>
+    )
+}
+
+export default ExampleComponent;
